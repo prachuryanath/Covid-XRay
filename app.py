fa410b666e630ff3abb44a07cf6e0460a16d30ba
Prachurya Nath <prachuryanath00@gmail.com>
2021-07-17 23:00:51 +0530

    Try error

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('xray_model.hdf5')
+  model = tf.keras.models.load_model('model')
   return model
+
 st.write("""
          # Covid Classification
          """
          )
 
 file = st.file_uploader("Please upload an xray file", type=["jpg", "png"])
-import opencv as cv2
+import cv2
 from PIL import Image, ImageOps
 import numpy as np
 st.set_option('deprecation.showfileUploaderEncoding', False)
 def import_and_predict(image_data, model):
     
-        size = (180,180)    
+        size = (128,128)    
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -28,14 +29,16 @@ def import_and_predict(image_data, model):
         prediction = model.predict(img_reshape)
         
         return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
+    model = load_model()
     predictions = import_and_predict(image, model)
     score = tf.nn.softmax(predictions[0])
-    st.write(prediction)
+    st.write(predictions)
     st.write(score)
     print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
