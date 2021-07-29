0f5a44e912c4733c812c76117017a9d71536fcf0
Prachurya Nath <prachuryanath00@gmail.com>
2021-07-29 22:12:00 +0530

    Model

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask, render_template, request
-from scipy.misc import imsave, imread, imresize
+import streamlit as st
+from utils import image_classification
+from PIL import Image
+from tensorflow.keras.models import load_model
+st.title("Covid 19 Prediction")
 
-app = Flask(__name__)
+st.text("Upload a XRAY Image for Covid classification as Covid or Non Covid")
 
-@app.route('/')
-def index_view():
-    return render_template('index.html')
+uploaded_file = st.file_uploader("Choose a XRAY Image ...", type=['jpg','jpeg','png'])
 
+@st.cache(allow_output_mutation=True)
+def load(model_path):
+    # Load the model
+    model = load_model(model_path)
+    return model
+    
+model = load('model')
 
-@app.route('/predict',methods=['GET','POST'])
-def predict():
-	response = "COVID 19 Prediction"
-return response	
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded XRAY.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    data = image_classification(image)
+    pred = model.predict(data)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    # st.write(label)
+    print(pred)
+    if pred < 0.5:
+        pred_acc = (0.5 - pred[0][0])/0.005
+        st.write("Prediction ACCURACY :",pred_acc)
+        st.write("COVID Positive")
+    else:
+        pred_acc = (pred[0][0]-0.5)*2
+        st.write("Prediction ACCURACY :",pred_acc)
+        st.write("COVID Negative")
--- utils.py
+++ utils.py
@@ -1,13 +1,9 @@
-import keras
 from PIL import Image, ImageOps
 import numpy as np
 from tensorflow.python.keras.backend import dtype
 
 
-def image_classification(img, model):
-    # Load the model
-    model = keras.models.load_model('model')
-
+def image_classification(img):
     # Create the array of the right shape to feed into the keras model
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     image = img
@@ -19,11 +15,12 @@ def image_classification(img, model):
     image = np.asarray(image, dtype=np.float16)
     image_array = image[:,:,:3]
     # Normalize the image
-    normalized_image_array = (image_array.astype(np.float16) / 127.0) - 1
+    normalized_image_array = image_array / 255.0
 
     # Load the image into the array
     data[0] = normalized_image_array
     
-    # run the inference
-    prediction = model.predict(data)
-    return np.argmax(prediction) # return position of the highest probability
+    return data
+    # # run the inference
+    # prediction = model.predict(data)
+    # return prediction # return position of the highest probability
